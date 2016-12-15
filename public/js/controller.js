6ec85498893b666e3ad271fcb01aad39fec38a43
Emil Widlund <hello@emilwidlund.com>
2016-12-15 11:13:21 +0100

    Added move actions to controller

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,6 +1,18 @@
+var socket = io();
+
 var actions = {
-  jump: function() {
+    moveForward: function() {
+        socket.emit('moveForward');
+        console.log('Move Forward');
+    },
+
+    moveBackward: function() {
+        socket.emit('moveBackward');
+        console.log('Move Backward');
+    },
+
+    jump: function() {
       socket.emit('jumpAction');
       console.log('Jump');
-  }
+    }
 };
