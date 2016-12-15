eddee7e9a11934a47bd59a0b1bc345efc60fb2cd
Emil Widlund <hello@emilwidlund.com>
2016-12-15 15:21:55 +0100

    Remove ability to add obstacles from controller

--- public/js/controller.js
+++ public/js/controller.js
@@ -14,13 +14,7 @@ var actions = {
     jump: function() {
       socket.emit('controller action', {action: 'jump'});
       console.log('Jump');
-    },
-
-    addObstacle: function() {
-        socket.emit('controller action', {action: 'addObstacle'});
-        console.log('Add Obstacle');
     }
 };
 
 document.getElementById('jump').addEventListener("touchstart", actions.jump, false);
-document.getElementById('addObstacle').addEventListener("touchstart", actions.addObstacle, false);
