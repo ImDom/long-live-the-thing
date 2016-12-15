f4442670d34fe6c4cfbdf94c29f1282851b02fa9
Emil Widlund <hello@emilwidlund.com>
2016-12-15 14:15:18 +0100

    Add addObstacle action in controller

--- public/js/controller.js
+++ public/js/controller.js
@@ -16,7 +16,13 @@ var actions = {
     jump: function() {
       socket.emit('controller action', {action: 'jump'});
       console.log('Jump');
+    },
+
+    addObstacle: function() {
+        socket.emit('controller action', {action: 'addObstacle'});
+        console.log('Add Obstacle');
     }
 };
 
 document.getElementById('jump').addEventListener("touchstart", actions.jump, false);
+document.getElementById('addObstacle').addEventListener("touchstart", actions.addObstacle, false);
